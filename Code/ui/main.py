54e6c38ee0f1e3dc386bdb68619f400ee664206f
Shubham Jadhav <shubhamjadhav1017@gmail.com>
2023-12-10 01:11:45 +0300

    Extract Text from pdf

--- Code/ui/main.py
+++ Code/ui/main.py
@@ -1,17 +1,5 @@
 import streamlit as st
-from PIL import Image
-
-def classify_job_role(resume_text):
-    # Placeholder for job role classification model
-    return "Software Engineer", 0.85
-
-def identify_skillsets(resume_text):
-    # Placeholder for skillset identification model
-    return ["Python", "Machine Learning", "Data Analysis"]
-
-def compare_job_description(resume_text, job_description):
-    # Placeholder for job description comparison model
-    return 0.75
+import helperfunctions as hf
 
 
 def main():
@@ -20,21 +8,21 @@ def main():
     # Upload and parse resume
     uploaded_file = st.file_uploader("Upload a Resume", type=["pdf", "docx"])
     if uploaded_file is not None:
-        resume_text = process_resume(uploaded_file)
+        resume_text, pages = hf.convert_pdf_to_txt_file(uploaded_file)
 
         # Job Role Classification
-        job_role, confidence = classify_job_role(resume_text)
+        job_role, confidence = hf.classify_job_role(resume_text)
         st.subheader(f"Predicted Job Role: {job_role} (Confidence: {confidence:.2%})")
 
         # Skillset Identification
-        skills = identify_skillsets(resume_text)
+        skills = hf.identify_skillsets(resume_text)
         st.subheader("Identified Skills:")
         st.write(", ".join(skills))
 
         # Job Description Comparison
         job_description = st.text_area("Enter Job Description:")
         if st.button("Compare"):
-            similarity_score = compare_job_description(resume_text, job_description)
+            similarity_score = hf.compare_job_description(resume_text, job_description)
             st.subheader(f"Similarity Score with Job Description: {similarity_score:.2%}")
 
         # Visualize relevant information using sliders
@@ -51,11 +39,6 @@ def main():
             confidence_threshold = st.slider("Job Role Confidence Threshold", 0.0, 1.0, 0.8, 0.01)
             st.write(f"Showing job roles with confidence >= {confidence_threshold:.2%}")
 
-
-def process_resume(uploaded_file):
-    # Placeholder for resume processing
-    resume_text = "Placeholder text extracted from resume."
-    return resume_text
 
 if __name__ == "__main__":
     main()
