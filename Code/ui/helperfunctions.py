9eeed8083858a5eaaa77ee95652e2ee0021aafea
JahnaviRamagiri <61361874+JahnaviRamagiri@users.noreply.github.com>
2023-12-10 23:25:43 -0500

    Code to Extract skillset

--- Code/ui/helperfunctions.py
+++ Code/ui/helperfunctions.py
@@ -5,6 +5,7 @@ from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 from io import StringIO
 from transformers import pipeline
+import re
 
 import pandas as pd
 from transformers import BertTokenizer, BertForSequenceClassification
@@ -12,13 +13,12 @@ import torch
 import numpy as np
 
 model_path = '../../Model/'
-
 device = "cpu"
 
 labels = np.load(model_path+'labels.npy', allow_pickle=True)
 
 model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=10).to(device)
-model.load_state_dict(torch.load(model_path+'resume_label.pth', map_location=torch.device(device)))
+model.load_state_dict(torch.load(model_path+'resume_label.pth'))
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
 
@@ -49,14 +49,37 @@ def classify_job_role(resume_text):
     # Placeholder for job role classification model
     return "Software Engineer", 0.85
 
+@st.cache_data
+def read_skillset():
+    df = pd.read_csv("../../Data/Cleaned/clean_norm_skillset.csv")
+    skills = ','.join(df['Skill'])
+    skillset = skills.split(',')
+    skillset = set(skillset)
+    skill_list = [element.strip() for element in skillset if len(element.split()) < 3]
+
+    return skill_list
+
+
+@st.cache_data
+def extract_skills_from_resume(text, skills_list):
+    skills = []
+
+    for skill in skills_list:
+        pattern = r"\b{}\b".format(re.escape(skill))
+        match = re.search(pattern, text, re.IGNORECASE)
+        if match:
+            skills.append(skill)
+
+    return skills
 
 @st.cache_data
 def identify_skillsets(resume_text):
-    # Placeholder for skillset identification model
-    return ["Python", "Machine Learning", "Data Analysis"]
+    skillset = read_skillset()
+    skills = extract_skills_from_resume(resume_text, skillset)
+    return skills
 
 
-@st.cache_data
+@st.cache_data()
 def compare_job_description(resume_text, job_description):
     # Placeholder for job description comparison model
     return 0.75
@@ -89,3 +112,5 @@ def summarize_text(text_input):
     summarized_output = summarizer(text_input,max_length=200)
     return summarized_output
     return text_input.summary_text
+
+
